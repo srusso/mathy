3219b347a324bcd3d8db541d2c6e4246c18dd57d
Simone Russo <simone@sr89.net>
2026-05-27 10:11:59 +0200

    Some multiplications.

---
+++ src/main/java/net/sr89/mathy/comp/CommutativeMatrices.java
@@ -0,0 +1,42 @@
+package net.sr89.mathy.comp;
+
+import net.sr89.mathy.obj.MutableSquareMatrix;
+
+public class CommutativeMatrices {
+    public static void main() {
+        var i = new MutableSquareMatrix(2);
+        var j = new MutableSquareMatrix(2);
+        var ij = new MutableSquareMatrix(2);
+        var ji = new MutableSquareMatrix(2);
+
+        long commutativePairs = 0;
+        long nonCommutativePairs = 0;
+
+        while(i.hasNext()) {
+            i.toNext();
+
+            while(j.hasNext()) {
+                j.toNext();
+
+                i.mult(j, ij);
+                j.mult(i, ji);
+
+                if(ij.equals(ji)) {
+                   commutativePairs++;
+                } else {
+                    nonCommutativePairs++;
+                }
+
+                if ((commutativePairs + nonCommutativePairs) % 100 == 0) {
+                    System.out.println("Done: " + (commutativePairs + nonCommutativePairs));
+                }
+            }
+
+            j.toFirst();
+        }
+
+        System.out.println("Total pairs: " + (commutativePairs + nonCommutativePairs));
+        System.out.println("Commutative pairs: " + commutativePairs);
+        System.out.println("Non commutative pairs: " + nonCommutativePairs);
+    }
+}
---
+++ src/main/java/net/sr89/mathy/obj/MutableSquareMatrix.java
@@ -0,0 +1,84 @@
+package net.sr89.mathy.obj;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class MutableSquareMatrix {
+    // min and max values for each element of the matrix
+    private static final int MIN = -5;
+    private static final int MAX = 5;
+
+    public final int[][] matrix;
+    private final int size;
+
+    // current row and column, used by toNext() and hasNext()
+    public int row;
+    public int column;
+
+    public MutableSquareMatrix(int size) {
+        this.matrix = new int[size][size];
+        this.size = size;
+        this.row = 0;
+        this.column = -1;
+        setAll(MIN);
+    }
+
+    public void mult(MutableSquareMatrix other, MutableSquareMatrix result) {
+        result.setAll(0);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++) {
+                    // possible optimization: surround this with if(result.matrix[i][j] != 0)
+                    // and remove setAll(0)
+                    result.matrix[i][j] += matrix[i][k] * other.matrix[k][j];
+                }
+            }
+        }
+    }
+
+    private void setAll(int value) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = value;
+            }
+        }
+    }
+
+    public void toFirst() {
+        row = 0;
+        column = -1;
+        setAll(MIN);
+    }
+
+    public void toNext() {
+        if (column == -1) {
+            column = 0;
+        } else if (matrix[row][column] < MAX) {
+            matrix[row][column]++;
+        } else if (column < size - 1) {
+            column++;
+        } else if (row < size - 1) {
+            row++;
+            column = 0;
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+    public boolean hasNext() {
+        return row < size - 1 || column < size - 1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        MutableSquareMatrix that = (MutableSquareMatrix) o;
+        return size == that.size && Objects.deepEquals(matrix, that.matrix);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Arrays.deepHashCode(matrix), size);
+    }
+}
