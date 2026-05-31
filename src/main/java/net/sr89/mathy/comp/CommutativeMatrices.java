1c6e0f27b61fc8a77b8cfc5102dba7be6c27d9c2
Simone Russo <simone@sr89.net>
2026-05-31 15:45:27 +0200

    Remove unused main method.

--- src/main/java/net/sr89/mathy/comp/CommutativeMatrices.java
+++ src/main/java/net/sr89/mathy/comp/CommutativeMatrices.java
@@ -65,43 +65,6 @@ public class CommutativeMatrices {
         System.out.println("Count (iterative): " + matrices2.size());
     }
 
-    public static void main2() {
-        var i = new MutableSquareMatrix(2);
-        var j = new MutableSquareMatrix(2);
-        var ij = new MutableSquareMatrix(2);
-        var ji = new MutableSquareMatrix(2);
-
-        long commutativePairs = 0;
-        long nonCommutativePairs = 0;
-
-        while(i.hasNext()) {
-            i.toNext();
-
-            while(j.hasNext()) {
-                j.toNext();
-
-                i.mult(j, ij);
-                j.mult(i, ji);
-
-                if(ij.equals(ji)) {
-                   commutativePairs++;
-                } else {
-                    nonCommutativePairs++;
-                }
-
-                if ((commutativePairs + nonCommutativePairs) % 100 == 0) {
-                    System.out.println("Done: " + (commutativePairs + nonCommutativePairs));
-                }
-            }
-
-            j.toFirst();
-        }
-
-        System.out.println("Total pairs: " + (commutativePairs + nonCommutativePairs));
-        System.out.println("Commutative pairs: " + commutativePairs);
-        System.out.println("Non commutative pairs: " + nonCommutativePairs);
-    }
-
     private static void printMatrix(MutableSquareMatrix matrix) {
         for (int i = 0; i < matrix.size; i++) {
             for (int j = 0; j < matrix.size; j++) {
