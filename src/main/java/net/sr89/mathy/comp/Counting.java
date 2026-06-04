f68c4c66f2dbff220cb78c4eba6f603a019c7d37
Simone Russo <simone@sr89.net>
2026-06-04 16:09:08 +0200

    Rename.

--- src/main/java/net/sr89/mathy/comp/Counting.java
+++ src/main/java/net/sr89/mathy/comp/Counting.java
@@ -5,21 +5,21 @@ package net.sr89.mathy.comp;
  */
 public class Counting {
     public static void main() {
-        int[] array = new int[3];
+        int[] digits = new int[3];
         final int highestDigit = 9;
 
-        printArray(array);
+        printArray(digits);
 
-        int i = array.length - 1;
+        int i = digits.length - 1;
 
         while (i >= 0) {
-            if (array[i] == highestDigit) {
-                array[i] = 0;
+            if (digits[i] == highestDigit) {
+                digits[i] = 0;
                 i--;
             } else {
-                array[i]++;
-                printArray(array);
-                i = array.length - 1;
+                digits[i]++;
+                printArray(digits);
+                i = digits.length - 1;
             }
         }
     }
